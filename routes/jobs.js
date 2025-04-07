fb5fdc84bd1f9803b6839efbaac2860fe9005919
mahamatoumaroumate <mhtoumate1999@gmail.com>
2025-04-07 16:32:53 +0300

    update GetStats

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,7 +1,7 @@
 const express=require('express')
 const router=express.Router()
 const{createJob,deleteJob,getJobsStats,getAllJobs,getJob,updateJob}=require('../controllers/jobs')
+router.get('/stats',getJobsStats)
 router.get('/',getAllJobs).post('/',createJob)
 router.get('/:id',getJob).patch('/:id',updateJob).delete('/:id',deleteJob)
-router.get('/stats',getJobsStats)
 module.exports=router
